e3a288c30e4a6010da328f783bd0116a64dc512e
Claudia Elisette <claudiae.urbinag@gmail.com>
2024-07-29 17:00:11 -0400

    Responsive Navbar

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+
+function showNavbar(){
+    document.getElementById("colapsable").style.display = "flex";
+    document.getElementById("colapsable").style.flexDirection= "column";
+}
+
+function hideNavbar(){
+    document.getElementById("colapsable").style.display = "none";
+}
